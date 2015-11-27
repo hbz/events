d6010f95c94e9c7a31020d0168f484a872622a9a
Fabian Steeg <fsteeg@gmail.com>
2015-11-27 14:05:53 +0100

    Basic routes and controllers setup

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,35 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.*;
 import play.mvc.*;
-
+import play.mvc.BodyParser.Json;
 import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public Result moved(String path) {
+		return movedPermanently("/" + path);
+	}
+
+	public Result index() {
+		return ok(index.render("POST or GET events: /events/:type"));
+	}
+
+	public Result getEvents(String type) {
+		String message = String.format("GET events: %s", type);
+		Logger.info(message);
+		return ok(message);
+	}
+
+	@BodyParser.Of(Json.class)
+	public Result addEvent(String type) {
+		JsonNode json = request().body().asJson();
+		String message =
+				String.format("POST event, type: %s, content: %s", type, json);
+		Logger.info(message);
+		return ok(message);
+	}
 
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -11,18 +11,18 @@ import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
-    @Test
-    public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertTrue(browser.pageSource().contains("Your new application is ready."));
-            }
-        });
-    }
+	/**
+	 * https://www.playframework.com/documentation/2.4.x/JavaTest
+	 */
+	@Test
+	public void test() {
+		running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT,
+				new Callback<TestBrowser>() {
+					public void invoke(TestBrowser browser) {
+						browser.goTo("http://localhost:3333/events");
+						assertTrue(browser.pageSource().contains("events"));
+					}
+				});
+	}
 
 }
